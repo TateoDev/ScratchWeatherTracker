af753c824743ba3fbe094671f22dd995600f4961
TateoDev <91488652+TateoDev@users.noreply.github.com>
2021-09-27 10:30:09 -0600

    Create WeatherTracker.py

---
+++ WeatherTracker.py
@@ -0,0 +1,16 @@
+from scratch2py import Scratch2Py
+import requests
+from time import sleep
+
+
+s2py = Scratch2Py('_tate', *PASSWORD*)
+
+cloudproject = s2py.scratchConnect('574764513')
+cloudproject.setCloudVar('CloudVar', 12)
+
+while True:
+  r = requests.get("https://api.openweathermap.org/data/2.5/weather?zip=83709,us&appid=*KEY=imperial")
+  r_json = r.json()
+  print(r_json["main"]["temp"])
+  cloudproject.setCloudVar('temp', r_json["main"]["temp"])
+  sleep(60)
